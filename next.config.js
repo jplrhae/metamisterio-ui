f519b1287891c0f8625028fb171c85d5504055f2
jplrhae <joshua.loesch@outlook.com>
2024-04-19 20:30:47 -0300

    Added icon to header, added meta data

--- next.config.js
+++ next.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   webpack: (config) => {
     config.resolve.alias.canvas = false;
-
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
     return config;
   },
 };
